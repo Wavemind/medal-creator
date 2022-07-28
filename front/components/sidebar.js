f0bc7359a23654daa61c2a12708c79103cbfee5c
Sinan Ucak <ucak.sinan@gmail.com>
2022-07-28 15:12:11 +0200

    added TODO

--- front/components/sidebar.js
+++ front/components/sidebar.js
@@ -53,7 +53,7 @@ const Sidebar = () => {
     >
       <VStack spacing={10}>
         <SidebarButton
-          // Get a better dynamic icon cos I can't change the color of this one
+          // TODO Get a better dynamic icon cos I can't change the color of this one
           icon={props => (
             <Image src={'/logoDynamic.svg'} alt="logo" height={12} {...props} />
           )}
