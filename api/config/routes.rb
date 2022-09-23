f434ffd9a0116be2e53c6a389dffb7b6de2edf3d
Quentin Girard <quentin.girard@wavemind.ch>
2022-09-23 14:24:14 +0200

    Setup rails config

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        sessions: 'api/v1/overrides/sessions'
+      }
+      namespace :webauthn do
+        resources :credentials, only: %i[index create destroy]
+        resources :challenges, only: %i[create]
+        resources :authentications, only: %i[create]
+      end
+    end
+  end
 end
