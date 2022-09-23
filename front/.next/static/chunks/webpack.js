b118b3e2df7240ee65ba033f2620f6f7af0fd0e0
Quentin Girard <quentin.girard@wavemind.ch>
2022-09-23 14:33:09 +0200

    Update gitignore

--- front/.next/static/chunks/webpack.js
+++ front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f04e4cae524e889c"; }
+/******/ 		__webpack_require__.h = function() { return "e7295985aee675b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
