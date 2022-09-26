fe1a5d3c895f98438bb6ae6d4946d6cff65d5a2d
Quentin Girard <quentin.girard@wavemind.ch>
2022-09-26 09:38:24 +0200

    Setup sign in

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -6,9 +6,8 @@ import { Box, ChakraProvider } from '@chakra-ui/react'
 import { createStandaloneToast } from '@chakra-ui/toast'
 import { appWithTranslation } from 'next-i18next'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/lib/integration/react'
-import { motion, AnimatePresence } from "framer-motion"
-import { useRouter } from "next/router"
+import { motion, AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 /**
  * Add fonts
@@ -27,49 +26,48 @@ import '@fontsource/ibm-plex-mono'
  */
 import theme from '/lib/theme'
 import Layout from '/lib/layouts/default'
-import { store, persistor } from '/lib/store'
+import { wrapper } from '../lib/store'
 
-function App({ Component, pageProps }) {
-  const router = useRouter();
+function App({ Component, ...rest }) {
+  const { store, props } = wrapper.useWrappedStore(rest)
+  const router = useRouter()
   const { ToastContainer } = createStandaloneToast()
-  const getLayout = Component.getLayout || (page => <Layout>{page}</Layout>)
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
 
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ChakraProvider theme={theme}>
-          <AnimatePresence exitBeforeEnter>
-            <Box
-              as={motion.div}
-              key={router.route}
-              initial="initialState"
-              animate="animateState"
-              exit="exitState"
-              w="100%"
-              minH="100vh"
-              transition={{
-                duration: 0.75,
-              }}
-              variants={{
-                initialState: {
-                  opacity: 0,
-                  clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-                },
-                animateState: {
-                  opacity: 1,
-                  clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-                },
-                exitState: {
-                  clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
-                },
-              }}
-            >
-              {getLayout(<Component {...pageProps} />)}
-            </Box>
-          </AnimatePresence>
-          <ToastContainer />
-        </ChakraProvider>
-      </PersistGate>
+      <ChakraProvider theme={theme}>
+        <AnimatePresence mode="wait">
+          <Box
+            as={motion.div}
+            key={router.route}
+            initial="initialState"
+            animate="animateState"
+            exit="exitState"
+            w="100%"
+            minH="100vh"
+            transition={{
+              duration: 0.75,
+            }}
+            variants={{
+              initialState: {
+                opacity: 0,
+                clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+              },
+              animateState: {
+                opacity: 1,
+                clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+              },
+              exitState: {
+                clipPath: 'polygon(50% 0, 50% 0, 50% 100%, 50% 100%)',
+              },
+            }}
+          >
+            {getLayout(<Component {...props.pageProps} />)}
+          </Box>
+        </AnimatePresence>
+        <ToastContainer />
+      </ChakraProvider>
     </Provider>
   )
 }
