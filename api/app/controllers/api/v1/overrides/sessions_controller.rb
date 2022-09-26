bbbc21bc7b43d9b899a5b42f02cd2a46f4436595
Quentin Girard <quentin.girard@wavemind.ch>
2022-09-26 15:06:27 +0200

    Add comments

--- api/app/controllers/api/v1/overrides/sessions_controller.rb
+++ api/app/controllers/api/v1/overrides/sessions_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::Overrides::SessionsController < DeviseTokenAuth::SessionsController
-
   def create
     # Check
     field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
@@ -11,17 +10,24 @@ class Api::V1::Overrides::SessionsController < DeviseTokenAuth::SessionsControll
       @resource = find_resource(field, q_value)
     end
 
-    if @resource && valid_params?(field, q_value) && (!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
+    # User is allowed to connect and is it valid ?
+    if @resource && valid_params?(field,
+                                  q_value) && (!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
       valid_password = @resource.valid_password?(resource_params[:password])
-      if (@resource.respond_to?(:valid_for_authentication?) && !@resource.valid_for_authentication? { valid_password }) || !valid_password
+      if (@resource.respond_to?(:valid_for_authentication?) && !@resource.valid_for_authentication? do
+            valid_password
+          end) || !valid_password
         return render_create_error_bad_credentials
       end
 
+      # Does user have 2FA activated
       if @resource.webauthn_credentials.present?
         get_options = WebAuthn::Credential.options_for_get(allow: @resource.webauthn_credentials.pluck(:external_id))
+        # Generate challenge for authentification
         $redis.hmset(@resource.id, 'authentication_challenge', get_options.challenge)
         render json: get_options
       else
+        # Auth user
         create_and_assign_token
         sign_in(:user, @resource, store: false, bypass: false)
         yield @resource if block_given?
@@ -29,6 +35,7 @@ class Api::V1::Overrides::SessionsController < DeviseTokenAuth::SessionsControll
       end
 
     elsif @resource && !(!@resource.respond_to?(:active_for_authentication?) || @resource.active_for_authentication?)
+      # Display error messages
       if @resource.respond_to?(:locked_at) && @resource.locked_at
         render_create_error_account_locked
       else
