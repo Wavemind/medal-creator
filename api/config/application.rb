38d86859dd35a6ae57301333c3fc3df23286af8e
Manu <emmanuel.barchichat@wavemind.ch>
2022-09-27 16:30:08 +0200

    implement tests

--- api/config/application.rb
+++ api/config/application.rb
@@ -1,10 +1,9 @@
 require_relative "boot"
 
 require "rails/all"
-require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you've limited to :spec, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module RailsGraphql
