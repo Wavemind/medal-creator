bc4054ad056f3db0624877618141e6b583e4ec7a
Quentin Girard <quentin.girard@wavemind.ch>
2022-09-28 14:16:56 +0200

    Finish migration of api

--- front/pages/api/credentials/authentification.js
+++ front/pages/api/credentials/authentification.js
@@ -1,56 +1,56 @@
-/**
- * The external imports
- */
-import { getCookie, setCookie } from 'cookies-next'
+// /**
+//  * The external imports
+//  */
+// import { getCookie, setCookie } from 'cookies-next'
 
-export default async function handler(req, res) {
-  const session = JSON.parse(getCookie('session', { req, res }))
+// export default async function handler(req, res) {
+//   const session = JSON.parse(getCookie('session', { req, res }))
 
-  let headers = new Headers()
-  headers.set('Accept', 'application/json')
-  headers.set('Content-Type', 'application/json')
-  headers.set('uid', session.uid)
-  headers.set('access-token', session.accessToken)
-  headers.set('client', session.client)
+//   let headers = new Headers()
+//   headers.set('Accept', 'application/json')
+//   headers.set('Content-Type', 'application/json')
+//   headers.set('uid', session.uid)
+//   headers.set('access-token', session.accessToken)
+//   headers.set('client', session.client)
 
-  const { body } = req
+//   const { body } = req
 
-  try {
-    // Auth user
-    const response = await fetch(
-      `${process.env.API_URL}/v1/webauthn/authentications`,
-      {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(body),
-      }
-    )
+//   try {
+//     // Auth user
+//     const response = await fetch(
+//       `${process.env.API_URL}/v1/webauthn/authentications`,
+//       {
+//         method: 'POST',
+//         headers: headers,
+//         body: JSON.stringify(body),
+//       }
+//     )
 
-    const data = await response.json()
+//     const data = await response.json()
 
-    if ([200, 201].includes(response.status)) {
-      setCookie(
-        'session',
-        JSON.stringify({
-          accessToken: response.headers.get('access-token'),
-          expiry: response.headers.get('expiry'),
-          uid: response.headers.get('uid'),
-          client: response.headers.get('client'),
-        }),
-        {
-          req,
-          res,
-          maxAge: 60 * 6 * 24,
-        }
-      )
-      res.status(200).json(data)
-      return
-    }
+//     if ([200, 201].includes(response.status)) {
+//       setCookie(
+//         'session',
+//         JSON.stringify({
+//           accessToken: response.headers.get('access-token'),
+//           expiry: response.headers.get('expiry'),
+//           uid: response.headers.get('uid'),
+//           client: response.headers.get('client'),
+//         }),
+//         {
+//           req,
+//           res,
+//           maxAge: 60 * 6 * 24,
+//         }
+//       )
+//       res.status(200).json(data)
+//       return
+//     }
 
-    res.status(response.status).send(data)
-  } catch (error) {
-    res.status(500).send({ error: error.message })
-  }
+//     res.status(response.status).send(data)
+//   } catch (error) {
+//     res.status(500).send({ error: error.message })
+//   }
 
-  res.end()
-}
+//   res.end()
+// }
--- front/pages/api/credentials/challenge.js
+++ front/pages/api/credentials/challenge.js
@@ -1,38 +1,38 @@
-/**
- * The external imports
- */
-import { getCookie } from 'cookies-next'
+// /**
+//  * The external imports
+//  */
+// import { getCookie } from 'cookies-next'
 
-export default async function handler(req, res) {
-  const session = JSON.parse(getCookie('session', { req, res }))
+// export default async function handler(req, res) {
+//   const session = JSON.parse(getCookie('session', { req, res }))
 
-  let headers = new Headers()
-  headers.set('Accept', 'application/json')
-  headers.set('Content-Type', 'application/json')
-  headers.set('uid', session.uid)
-  headers.set('access-token', session.accessToken)
-  headers.set('client', session.client)
+//   let headers = new Headers()
+//   headers.set('Accept', 'application/json')
+//   headers.set('Content-Type', 'application/json')
+//   headers.set('uid', session.uid)
+//   headers.set('access-token', session.accessToken)
+//   headers.set('client', session.client)
 
-  try {
-    const response = await fetch(
-      `${process.env.API_URL}/v1/webauthn/challenges`,
-      {
-        method: 'POST',
-        headers: headers,
-      }
-    )
+//   try {
+//     const response = await fetch(
+//       `${process.env.API_URL}/v1/webauthn/challenges`,
+//       {
+//         method: 'POST',
+//         headers: headers,
+//       }
+//     )
 
-    const data = await response.json()
+//     const data = await response.json()
 
-    if ([200, 201].includes(response.status)) {
-      res.status(200).json(data)
-      return
-    }
+//     if ([200, 201].includes(response.status)) {
+//       res.status(200).json(data)
+//       return
+//     }
 
-    res.status(response.status).send(data)
-  } catch (error) {
-    res.status(500).send({ error: error.message })
-  }
+//     res.status(response.status).send(data)
+//   } catch (error) {
+//     res.status(500).send({ error: error.message })
+//   }
 
-  res.end()
-}
+//   res.end()
+// }
