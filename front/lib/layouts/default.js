f3b4e4f8485e54d12262e17cdae8a2f3eacf53d1
Quentin Girard <quentin.girard@wavemind.ch>
2022-10-03 11:48:18 +0200

    Setup alert dialog and add it to 2fa

--- front/lib/layouts/default.js
+++ front/lib/layouts/default.js
@@ -6,49 +6,67 @@ import { Flex, Image, useTheme, Box } from '@chakra-ui/react'
 /**
  * The internal imports
  */
-import { Sidebar, UserMenu, SubMenu } from '../../components'
+import { Sidebar, UserMenu, SubMenu, AlertDialog } from '/components'
+import { AlertDialogContext } from '../contexts'
+import useAlertDialogModal from '../hooks/useAlertDialogModal'
 
 const Layout = ({ children, menuType = null }) => {
   const { colors, dimensions } = useTheme()
 
+  const {
+    isOpenAlertDialog,
+    openAlertDialog,
+    closeAlertDialog,
+    alertDialogContent,
+  } = useAlertDialogModal()
+
   return (
-    <Box width="100%">
+    <Box width='100%'>
       <Flex
         bg={colors.primary}
         height={dimensions.headerHeight}
-        width="100%"
-        alignItems="center"
-        justifyContent="space-between"
+        width='100%'
+        alignItems='center'
+        justifyContent='space-between'
         paddingRight={5}
         paddingLeft={5}
-        position="fixed"
-        zIndex={9999}
+        position='fixed'
       >
-        <Image src={'/logo.svg'} alt="logo" height={14} />
+        <Image src={'/logo.svg'} alt='logo' height={14} />
         <UserMenu />
       </Flex>
       <Flex>
         <Sidebar />
         {menuType && <SubMenu menuType={menuType} />}
         <Box
-          position="fixed"
+          position='fixed'
           left={
-            menuType ?
-              `calc(${dimensions.sidebarWidth} + ${dimensions.subMenuWidth})` :
-              dimensions.sidebarWidth
+            menuType
+              ? `calc(${dimensions.sidebarWidth} + ${dimensions.subMenuWidth})`
+              : dimensions.sidebarWidth
           }
           top={dimensions.headerHeight}
           padding={10}
           height={`calc(100% - ${dimensions.headerHeight})`}
           width={
-            menuType ?
-              `calc(100% - ${dimensions.sidebarWidth} - ${dimensions.subMenuWidth})` :
-              `calc(100% - ${dimensions.sidebarWidth})`
+            menuType
+              ? `calc(100% - ${dimensions.sidebarWidth} - ${dimensions.subMenuWidth})`
+              : `calc(100% - ${dimensions.sidebarWidth})`
           }
-          overflowY="visible"
-          overflowX="hidden"
+          overflowY='visible'
+          overflowX='hidden'
         >
-          {children}
+          <AlertDialogContext.Provider
+            value={{
+              isOpenAlertDialog,
+              openAlertDialog,
+              closeAlertDialog,
+              alertDialogContent,
+            }}
+          >
+            {children}
+            <AlertDialog />
+          </AlertDialogContext.Provider>
         </Box>
       </Flex>
     </Box>
