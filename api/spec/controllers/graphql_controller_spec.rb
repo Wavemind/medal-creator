1d1ec202e3a5543e590f106d15dd29602c2ac6f4
Alain Fresco <alain.fresco@gmail.com>
2022-10-04 13:36:43 +0200

    :white_check_mark: Fix  users tests

--- api/spec/controllers/graphql_controller_spec.rb
+++ api/spec/controllers/graphql_controller_spec.rb
@@ -5,7 +5,7 @@ describe Api::V1::GraphqlController, type: :controller do
     user = User.create!(first_name: 'Manu', last_name: 'Girard', email: 'manu.girard@wavemind.ch', password: '123456')
     query = <<-GRAPHQL
       query{
-        getUser(id: 1){
+        getUser(id: 2){
           firstName
           lastName
         }
