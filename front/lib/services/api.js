686a421af0d05c7c41fa1c57eadbd2641f8e6d98
Quentin Girard <quentin.girard@wavemind.ch>
2022-10-04 11:06:45 +0200

    Clean

--- front/lib/services/api.js
+++ front/lib/services/api.js
@@ -10,7 +10,6 @@ import { getCookie, hasCookie } from 'cookies-next'
  */
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_API_URL,
-  // mode: 'no-cors',
   prepareHeaders: async (headers, { getState }) => {
     let session = ''
     if (hasCookie('session')) {
