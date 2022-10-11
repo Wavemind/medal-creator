7372c4d7e44bde5396cf6d8957c687c65f3911d9
Quentin Girard <quentin.girard@wavemind.ch>
2022-10-11 16:40:00 +0200

    lint

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,7 +1,7 @@
 /**
  * The external imports
  */
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createStandaloneToast } from '@chakra-ui/toast'
 import { appWithTranslation } from 'next-i18next'
