52ccfef9de12402444bb7319c7b6a846226453c5
Quentin Girard <quentin.girard@wavemind.ch>
2022-10-11 16:23:21 +0200

    Add error for testing

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,7 +1,7 @@
 /**
  * The external imports
  */
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createStandaloneToast } from '@chakra-ui/toast'
 import { appWithTranslation } from 'next-i18next'
