ba4c85d4edd3564fdbfc05b9fafa68b77bfaa944
Quentin Girard <quentin.girard@wavemind.ch>
2022-10-12 15:07:05 +0200

    Add devise invitable, add tests and add devise invitable in back

---
+++ api/app/graphql/mutations/users/create_user.rb
@@ -0,0 +1,28 @@
+module Mutations
+  module Users
+    class CreateUser < Mutations::BaseMutation
+      # Fields
+      field :user, Types::UserType, null: false
+      field :errors, [String], null: false
+
+      # Arguments
+      argument :params, Types::Input::UserInputType, required: true
+
+      # Resolve
+      def resolve(params:)
+        user_params = Hash params
+        begin
+          user = User.new(user_params)
+          if user.valid?
+            { user: User.invite!(user_params) }
+          else
+            GraphQL::ExecutionError.new(user.errors.full_messages.join(', '))
+          end
+        rescue ActiveRecord::RecordInvalid => e
+          GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+            " #{e.record.errors.full_messages.join(', ')}")
+        end
+      end
+    end
+  end
+end
