21303dede5287627437adb5dd7a70d27ef4bfe0a
Manu <emmanuel.barchichat@wavemind.ch>
2022-11-04 16:27:38 +0100

    WIP

--- api/app/graphql/types/input/algorithm_input_type.rb
+++ api/app/graphql/types/input/algorithm_input_type.rb
@@ -4,8 +4,8 @@ module Types
       argument :name, String, required: false
       argument :minimum_age, Integer, required: false
       argument :age_limit, Integer, required: false
-      argument :age_limit_message_translations, Types::HstoreType, required: false
-      argument :description_translations, Types::HstoreType, required: false
+      argument :age_limit_message_translations, Types::Input::HstoreInputType, required: false
+      argument :description_translations, Types::Input::HstoreInputType, required: false
       argument :mode, String, required: false
       argument :status, String, required: false
       argument :full_order_json, GraphQL::Types::JSON, required: false
