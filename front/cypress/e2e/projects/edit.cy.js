f8e72a35f1aa1624138a147f7937fcbd9e5b23d6
Quentin Girard <quentin_girard@hotmail.com>
2022-11-22 11:00:30 +0100

    Update test

--- front/cypress/e2e/projects/edit.cy.js
+++ front/cypress/e2e/projects/edit.cy.js
@@ -1,11 +1,5 @@
 /* eslint-disable no-undef */
 describe('Update a project page', () => {
-  it("should be redirect to '/' because he doesn't have access", () => {
-    cy.login('/projects/1/edit')
-    cy.get('h2').should('contain', 'Projects')
-    cy.logout()
-  })
-
   it('should navigate to projects/1/edit', () => {
     cy.loginAsAdmin()
     cy.visit('/projects/1/edit')
