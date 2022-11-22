23aa9ce2948c5d3ebc13bd6913fccce0073aefae
Sinan Ucak <ucak.sinan@gmail.com>
2022-11-22 16:33:24 +0100

    need to merge other branches

---
+++ front/pages/users.js
@@ -0,0 +1,40 @@
+/**
+ * The external imports
+ */
+import { useContext } from 'react'
+import { Heading, Button, HStack } from '@chakra-ui/react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+/**
+ * The internal imports
+ */
+import { ModalContext } from '/lib/contexts'
+import { CreateAlgorithmForm } from '/components'
+import { useTranslation } from 'next-i18next'
+
+export default function Users() {
+  const { t } = useTranslation('users')
+  const { openModal } = useContext(ModalContext)
+
+  const handleOpenModal = () => {
+    openModal({
+      title: t('create'),
+      content: <CreateAlgorithmForm />,
+    })
+  }
+
+  return (
+    <HStack justifyContent='space-between'>
+      <Heading as='h1'>{t('heading')}</Heading>
+      <Button data-cy='create_user' onClick={handleOpenModal}>
+        {t('create')}
+      </Button>
+    </HStack>
+  )
+}
+
+export const getServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common', 'users'])),
+  },
+})
