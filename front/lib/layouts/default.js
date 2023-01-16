72ba769aa764503c827a72ca0fc33adda811e7e8
Quentin Girard <quentin_girard@hotmail.com>
2023-01-16 17:36:16 +0100

    NextJS, Chakra-UI, Next-Redux-Wrapper, Redux dump

--- front/lib/layouts/default.js
+++ front/lib/layouts/default.js
@@ -2,7 +2,7 @@
  * The external imports
  */
 import { Flex, useTheme, Box, Select, HStack } from '@chakra-ui/react'
-import Image from 'next/future/image'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 /**
