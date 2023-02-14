4d4f993620b47702ee200e2d0c47a74b32533e1e
Quentin Girard <quentin_girard@hotmail.com>
2023-02-14 17:30:12 +0100

    Add formError component

---
+++ front/components/formError.tsx
@@ -0,0 +1,39 @@
+/**
+ * The external imports
+ */
+import { FC, useMemo } from 'react'
+import { Text } from '@chakra-ui/react'
+
+/**
+ * Type definitions
+ */
+import type { SerializedError } from '@reduxjs/toolkit'
+import type { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query'
+import {
+  isFetchBaseQueryError,
+  isErrorWithMessage,
+} from '@/lib/utils/errorsHelpers'
+
+type FormErrorProps = {
+  error: SerializedError | FetchBaseQueryError | unknown
+}
+
+const FormError: FC<FormErrorProps> = ({ error }) => {
+  const errorMessage = useMemo(() => {
+    if (isFetchBaseQueryError(error)) {
+      return 'error' in error ? error.error : error.data.errors.join()
+    } else if (isErrorWithMessage(error)) {
+      return error.message
+    } else {
+      return ''
+    }
+  }, [error])
+
+  return (
+    <Text fontSize='m' color='red' data-cy='server_message'>
+      {errorMessage}
+    </Text>
+  )
+}
+
+export default FormError
