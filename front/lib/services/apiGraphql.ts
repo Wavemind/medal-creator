c457fade63495c15e9cb063fbf0cf14bfd711b75
Quentin Girard <quentin_girard@hotmail.com>
2023-02-15 15:18:32 +0100

    Migrate apiGraph

--- front/lib/services/apiGraphql.ts
+++ front/lib/services/apiGraphql.ts
@@ -3,24 +3,17 @@
  */
 import { createApi } from '@reduxjs/toolkit/query/react'
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
-import { GraphQLClient } from 'graphql-request'
 import { HYDRATE } from 'next-redux-wrapper'
 
 /**
  * The internal imports
  */
-import prepareHeaders from '../utils/prepareHeaders'
-
-const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_API_URL}/graphql`, {
-  headers: {
-    Accept: 'application/json',
-  },
-})
+import prepareHeaders from '@/lib/utils/prepareHeaders'
 
 export const apiGraphql = createApi({
   reducerPath: 'apiGraphql',
   baseQuery: graphqlRequestBaseQuery({
-    client,
+    url: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
     prepareHeaders: prepareHeaders,
   }),
   refetchOnMountOrArgChange: 10,
