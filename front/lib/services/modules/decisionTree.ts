b1bf78cfdaeccd9b85ef6bd0ef0b7abc388b52f2
Quentin Girard <quentin.girard@wavemind.ch>
2023-02-20 14:34:09 +0100

    Migrate decisionTree

--- front/lib/services/modules/decisionTree.ts
+++ front/lib/services/modules/decisionTree.ts
@@ -2,11 +2,14 @@
  * The internal imports
  */
 import { apiGraphql } from '../apiGraphql'
-import getDecisionTreesQuery from './decisionTree/getDecisionTrees'
-import createDecisionTreeMutation from './decisionTree/createDecisionTree'
-import updateDecisionTreeMutation from './decisionTree/updateDecisionTree'
-import { getDecisionTreeDocument } from './documents/decisionTree'
-import { DecisionTree } from '@/types/decisionTree'
+import {
+  createDecisionTreeDocument,
+  getDecisionTreeDocument,
+  getDecisionTreesDocument,
+  updateDecisionTreeDocument,
+} from './documents/decisionTree'
+import type { DecisionTree } from '@/types/decisionTree'
+import type { Paginated } from '@/types/common'
 
 export const decisionTreesApi = apiGraphql.injectEndpoints({
   endpoints: build => ({
@@ -16,12 +19,44 @@ export const decisionTreesApi = apiGraphql.injectEndpoints({
         variables: { id },
       }),
       transformResponse: (response: { getDecisionTree: DecisionTree }) =>
-      response.getDecisionTree,
+        response.getDecisionTree,
       providesTags: ['DecisionTree'],
     }),
-    getDecisionTrees: getDecisionTreesQuery(build),
-    createDecisionTree: createDecisionTreeMutation(build),
-    updateDecisionTree: updateDecisionTreeMutation(build),
+    getDecisionTrees: build.query<Paginated<DecisionTree>, { search?: string }>(
+      {
+        query: ({ search }) => ({
+          document: getDecisionTreesDocument,
+          variables: { searchTerm: search },
+        }),
+        transformResponse: (response: {
+          getDecisionTrees: Paginated<DecisionTree>
+        }) => response.getDecisionTrees,
+        providesTags: ['DecisionTree'],
+      }
+    ),
+    createDecisionTree: build.mutation<DecisionTree, DecisionTreeInputs>({
+      query: values => ({
+        document: createDecisionTreeDocument,
+        variables: values,
+      }),
+      transformResponse: (response: {
+        createDecisionTree: { decisionTree: DecisionTree }
+      }) => response.createDecisionTree.decisionTree,
+      invalidatesTags: ['DecisionTree'],
+    }),
+    updateDecisionTree: build.mutation<
+      DecisionTree,
+      Partial<DecisionTreeInputs> & Pick<DecisionTree, 'id'>
+    >({
+      query: values => ({
+        document: updateDecisionTreeDocument,
+        variables: values,
+      }),
+      transformResponse: (response: {
+        updateDecisionTree: { decisionTree: DecisionTree }
+      }) => response.updateDecisionTree.decisionTree,
+      invalidatesTags: ['DecisionTree'],
+    }),
   }),
   overrideExisting: false,
 })
