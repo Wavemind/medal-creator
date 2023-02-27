217a7a311ed3e8f4d08e6e7e95021b92e9b0c412
Manu <emmanuel.barchichat@wavemind.ch>
2023-02-27 14:52:03 +0100

    Update decision tree tests

--- api/spec/graphql/mutations/decision_trees/destroy_decision_tree_spec.rb
+++ api/spec/graphql/mutations/decision_trees/destroy_decision_tree_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 module Mutations
-  module Diagnoses
-    describe DestroyDiagnosis, type: :request do
+  module DecisionTrees
+    describe DestroyDecisionTree, type: :request do
       describe '.resolve' do
         it 'Removes components conditions and children in cascade' do
           expect do
             post '/graphql',
                  params: { query: query }
-          end.to change { Node.count }.by(-1)
-             .and change { Instance.count }.by(-4)
+          end.to change { Node.count }.by(-2)
+             .and change { DecisionTree.count }.by(-1)
+             .and change { Instance.count }.by(-6)
              .and change { Condition.count }.by(-6)
              .and change { Child.count }.by(-5)
         end
@@ -18,9 +19,9 @@ module Mutations
       def query
         <<~GQL
           mutation {
-            destroyDiagnosis(
+            destroyDecisionTree(
               input: {
-                id: #{Diagnosis.first.id}
+                id: #{DecisionTree.first.id}
             }){
               id
             } 
