d79d92b24c9f6a0c4423cae828adb249ef50b600
Quentin Girard <quentin_girard@hotmail.com>
2023-02-28 17:08:54 +0100

    Add next-auth config and setup middleware

--- front/pages/api/auth/[...nextauth].ts
+++ front/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,14 @@
-import NextAuth from "next-auth"
+/**
+ * The external imports
+ */
+import NextAuth from 'next-auth'
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [],
+  pages: {
+    signIn: '/auth/sign-in',
+  },
 }
 
 export default NextAuth(authOptions)
