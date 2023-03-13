f78a1b21a776842628c246a0f996493afc57b520
Quentin Girard <quentin.girard@wavemind.ch>
2023-03-13 12:22:17 +0100

    Fix cut off validations

--- api/app/models/decision_tree.rb
+++ api/app/models/decision_tree.rb
@@ -13,24 +13,32 @@ class DecisionTree < ApplicationRecord
                                                                         } }
   validates :cut_off_start, numericality: true, allow_nil: true
   validates :cut_off_end, numericality: true, allow_nil: true
-  validates_comparison_of :cut_off_start,
-                          less_than: :cut_off_end, allow_nil: true
+  validate :cut_off_start_less_than_cut_off_end
 
   before_validation :adjust_cut_offs
 
   translates :label
-
-  # Adjust cut offs at creation
-  def adjust_cut_offs
-    self.cut_off_start = (cut_off_start * 30.4166667).round if cut_off_start.present? && cut_off_value_type == 'months'
-    self.cut_off_end = (cut_off_end * 30.4166667).round if cut_off_end.present? && cut_off_value_type == 'months'
-    self.cut_off_value_type = '' # Empty attr accessor to prevent callbacks to falsely do the operation more than once
-  end
 
   # Search by label (hstore) for the project language
   def self.search(term, language)
     joins(:diagnoses).where(
       'decision_trees.label_translations -> :l ILIKE :search OR nodes.label_translations -> :l ILIKE :search', l: language, search: "%#{term}%"
     ).distinct
+  end
+
+  private
+
+  # TODO: MIGRATE IT IN VALIDATION, NEED TO BE USED IN QS
+  def cut_off_start_less_than_cut_off_end
+    if cut_off_start.present? && cut_off_end.present? && cut_off_start >= cut_off_end
+      errors.add(:cut_off_start, 'must be less than cut_off_end')
+    end
+  end
+
+  # Adjust cut offs at creation
+  def adjust_cut_offs
+    self.cut_off_start = (cut_off_start * 30.4166667).round if cut_off_start.present? && cut_off_value_type == 'months'
+    self.cut_off_end = (cut_off_end * 30.4166667).round if cut_off_end.present? && cut_off_value_type == 'months'
+    self.cut_off_value_type = '' # Empty attr accessor to prevent callbacks to falsely do the operation more than once
   end
 end
