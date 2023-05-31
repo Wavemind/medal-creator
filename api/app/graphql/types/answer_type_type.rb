03fba71eed221cd8905e32494121c3cf0384cf1f
Quentin Girard <quentin_girard@hotmail.com>
2023-05-31 15:26:16 +0200

    Fix merge

--- api/app/graphql/types/answer_type_type.rb
+++ api/app/graphql/types/answer_type_type.rb
@@ -1,12 +1,7 @@
 module Types
   class AnswerTypeType < Types::BaseObject
-<<<<<<< HEAD
-    field :display, String, resolver_method: :resolve_display
-    field :value, String
-    field :label_key, String
-=======
     field :display, String, resolver_method: :resolve_display, null: false
     field :value, String, null: false
->>>>>>> feature/create-qs
+    field :label_key, String
   end
 end
