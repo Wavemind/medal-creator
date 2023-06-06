35539b6c43b0cfd4ec9e783cb5c7fc3036661cb6
ManuBarchich <manu250493@yahoo.fr>
2023-06-06 05:59:54 +0200

    Add queries for managements and qsses

---
+++ api/app/graphql/queries/managements/get_management.rb
@@ -0,0 +1,29 @@
+module Queries
+  module Managements
+    class GetManagement < Queries::BaseQuery
+      type Types::ManagementType, null: true
+      argument :id, ID
+
+      # Works with current_user
+      def authorized?(id:)
+        management = HealthCares::Management.find(id)
+
+        if context[:current_api_v1_user].admin? || context[:current_api_v1_user].user_projects.where(project_id: management.project_id).any?
+          return true
+        end
+
+        raise GraphQL::ExecutionError, I18n.t('graphql.errors.wrong_access', class_name: 'Management')
+      rescue ActiveRecord::RecordNotFound => e
+        GraphQL::ExecutionError.new(I18n.t('graphql.errors.object_not_found', class_name: e.model))
+      end
+
+      def resolve(id:)
+        HealthCares::Management.find(id)
+      rescue ActiveRecord::RecordNotFound => e
+        GraphQL::ExecutionError.new(I18n.t('graphql.errors.object_not_found', class_name: e.model))
+      rescue ActiveRecord::RecordInvalid => e
+        GraphQL::ExecutionError.new(e.record.errors.to_json)
+      end
+    end
+  end
+end
---
+++ api/app/graphql/queries/managements/get_managements.rb
@@ -0,0 +1,31 @@
+module Queries
+  module Managements
+    class GetManagements < Queries::BaseQuery
+      type Types::ManagementType.connection_type, null: false
+      argument :project_id, ID
+      argument :search_term, String, required: false
+
+      # Works with current_user
+      def authorized?(project_id:, search_term: '')
+        if context[:current_api_v1_user].admin? || context[:current_api_v1_user].user_projects.where(project_id: project_id).any?
+          return true
+        end
+
+        raise GraphQL::ExecutionError, I18n.t('graphql.errors.wrong_access', class_name: 'Project')
+      end
+
+      def resolve(project_id:, search_term: '')
+        project = Project.find(project_id)
+        if search_term.present?
+          project.managements.includes(:instances).search(search_term, project.language.code)
+        else
+          project.managements.includes(:instances).order(updated_at: :desc)
+        end
+      rescue ActiveRecord::RecordNotFound => e
+        GraphQL::ExecutionError.new(I18n.t('graphql.errors.object_not_found', class_name: e.record.class))
+      rescue ActiveRecord::RecordInvalid => e
+        GraphQL::ExecutionError.new(e.record.errors.to_json)
+      end
+    end
+  end
+end
