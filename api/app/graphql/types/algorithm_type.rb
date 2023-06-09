c653a7b6d2ac3caaf13fd03c1dccef009e4b3d73
Sinan Ucak <ucak.sinan@gmail.com>
2023-06-09 08:48:46 +0200

    forgot to push the back

--- api/app/graphql/types/algorithm_type.rb
+++ api/app/graphql/types/algorithm_type.rb
@@ -5,7 +5,7 @@ module Types
     field :age_limit, Integer, null: false
     field :age_limit_message_translations, Types::HstoreType, null: false
     field :description_translations, Types::HstoreType, null: false
-    field :mode, String, null: false
+    field :mode, String
     field :status, String, null: false
     field :full_order_json, GraphQL::Types::JSON
     field :medal_r_json, GraphQL::Types::JSON
