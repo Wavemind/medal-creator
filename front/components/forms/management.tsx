66ffa2a2052bd41b3093a837f3efc50c0ac978ff
Quentin Girard <quentin_girard@hotmail.com>
2023-06-13 09:37:05 +0200

    Form inputs added

--- front/components/forms/management.tsx
+++ front/components/forms/management.tsx
@@ -2,19 +2,34 @@
  * The external imports
  */
 import { useTranslation } from 'next-i18next'
-import { Spinner, Text } from '@chakra-ui/react'
+import { Button, HStack, Spinner, VStack } from '@chakra-ui/react'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 
 /**
  * The internal imports
  */
+import {
+  Checkbox,
+  Dropzone,
+  FormProvider,
+  Input,
+  Slider,
+  Textarea,
+} from '@/components'
 import { useGetProjectQuery } from '@/lib/api/modules'
 import type { ManagementFormComponent, ManagementInputs } from '@/types'
-import { useForm } from 'react-hook-form'
+import { FILE_EXTENSIONS_AUTHORIZED } from '@/lib/config/constants'
+import { useState } from 'react'
 
 const ManagementForm: ManagementFormComponent = ({ projectId }) => {
   const { t } = useTranslation('managements')
+
+  const [filesToAdd, setFilesToAdd] = useState<File[]>([])
+  const [existingFilesToRemove, setExistingFilesToRemove] = useState<number[]>(
+    []
+  )
 
   const { data: project, isSuccess: isGetProjectSuccess } =
     useGetProjectQuery(projectId)
@@ -35,12 +50,79 @@ const ManagementForm: ManagementFormComponent = ({ projectId }) => {
       description: '',
       levelOfUrgency: 5,
       isReferral: false,
+      isNeonat: false,
       projectId: projectId,
     },
   })
 
+  /**
+   * Create or update a management with data passed in params
+   * @param {} data
+   */
+  const onSubmit: SubmitHandler<ManagementInputs> = data => {
+    console.log(data)
+  }
+
   if (isGetProjectSuccess) {
-    return <Text>Hello</Text>
+    return (
+      <FormProvider<ManagementInputs>
+        methods={methods}
+        isError={false}
+        error={{}}
+      >
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <VStack align='left' spacing={8}>
+            <Input
+              name='label'
+              label={t('label')}
+              isRequired
+              helperText={t('helperText', {
+                language: t(`languages.${project.language.code}`, {
+                  ns: 'common',
+                  defaultValue: '',
+                }),
+                ns: 'common',
+              })}
+            />
+            <Textarea
+              name='description'
+              label={t('description')}
+              helperText={t('helperText', {
+                language: t(`languages.${project.language.code}`, {
+                  ns: 'common',
+                  defaultValue: '',
+                }),
+                ns: 'common',
+              })}
+            />
+            <Checkbox label={t('isNeonat')} name='isPreFill' />
+            <Checkbox label={t('isReferral')} name='isNeonat' />
+            <Slider name='levelOfUrgency' label={t('levelOfUrgency')} />
+            <Dropzone
+              label={t('dropzone.mediaUpload', { ns: 'common' })}
+              name='mediaUpload'
+              multiple
+              acceptedFileTypes={FILE_EXTENSIONS_AUTHORIZED}
+              existingFiles={[]} // TODO ADD CURRENT EXISTING FILES
+              setExistingFilesToRemove={setExistingFilesToRemove}
+              existingFilesToRemove={existingFilesToRemove}
+              filesToAdd={filesToAdd}
+              setFilesToAdd={setFilesToAdd}
+            />
+            <HStack justifyContent='flex-end'>
+              <Button
+                type='submit'
+                data-cy='submit'
+                mt={6}
+                // isLoading={isCreateDiagnosisLoading || isUpdateDiagnosisLoading}
+              >
+                {t('save', { ns: 'common' })}
+              </Button>
+            </HStack>
+          </VStack>
+        </form>
+      </FormProvider>
+    )
   }
 
   return <Spinner size='xl' />
