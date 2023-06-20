3fc1d986fde2eab96a83f60cbd1463e67c91aed9
ManuBarchich <manu250493@yahoo.fr>
2023-06-20 09:04:18 +0200

    Create all administration routes, fix formulation creation and move files test to management

--- api/app/graphql/mutations/drugs/update_drug.rb
+++ api/app/graphql/mutations/drugs/update_drug.rb
@@ -6,11 +6,9 @@ module Mutations
 
       # Arguments
       argument :params, Types::Input::DrugInputType, required: true
-      argument :files_to_add, [ApolloUploadServer::Upload], required: false
-      argument :existing_files_to_remove, [Int], required: false
 
       # Works with current_user
-      def authorized?(params:, files_to_add:, existing_files_to_remove:)
+      def authorized?(params:)
         project_id = Hash(params)[:project_id]
         return true if context[:current_api_v1_user].clinician? || context[:current_api_v1_user].user_projects.where(
           project_id: project_id, is_admin: true
@@ -20,16 +18,12 @@ module Mutations
       end
 
       # Resolve
-      def resolve(params:, files_to_add:, existing_files_to_remove:)
+      def resolve(params:)
         drug_params = Hash params
         ActiveRecord::Base.transaction(requires_new: true) do
           begin
             drug = HealthCares::Drug.find(drug_params[:id])
             if drug.update!(drug_params)
-              files_to_add.each do |file|
-                drug.files.attach(io: file, filename: file.original_filename)
-              end
-              ActiveStorage::Attachment.destroy(existing_files_to_remove) if existing_files_to_remove.any?
               { drug: drug }
             else
               raise GraphQL::ExecutionError.new(drug.errors.to_json)
