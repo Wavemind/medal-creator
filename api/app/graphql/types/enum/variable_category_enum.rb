a14de8f64a9fd2497b93093268af9b607eb8a1c9
ManuBarchich <manu250493@yahoo.fr>
2023-08-04 14:49:46 +0200

    Add dummy query to implement enums for included categories by diagram

--- api/app/graphql/types/enum/variable_category_enum.rb
+++ api/app/graphql/types/enum/variable_category_enum.rb
@@ -5,7 +5,8 @@ module Types
         value option.gsub('Variables::', '')
       end
 
-      def self.coerce_input(input_value, _context)
+      def self.coerce_input(input_value, context)
+        process_input(input_value, context)
         transformed_value = "Variables::#{input_value}"
         validate_enum_value(transformed_value)
         transformed_value
@@ -20,6 +21,17 @@ module Types
         return if allowed_values.include?(value)
 
         raise GraphQL::CoercionError, "Invalid enum value: #{value}"
+      end
+
+      private
+
+      def self.process_input(input_value, context)
+        puts '********'
+        puts context[:current_user]
+        puts '********'
+        # Use context information to process the input
+        # For example, access context[:current_user]
+        # Return the processed input value
       end
     end
   end
