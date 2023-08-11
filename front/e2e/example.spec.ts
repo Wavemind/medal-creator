83175321eeb7ba10858b572e75970d6440e1bd76
Alain Fresco <alain.fresco@gmail.com>
2023-08-11 13:33:29 +0200

    :construction: Pour Quentin

--- front/e2e/example.spec.ts
+++ front/e2e/example.spec.ts
@@ -10,3 +10,58 @@ test('should navigate to the login page', async ({ page }) => {
   // The new page should contain an h1 with "About Page"
   // await expect(page.locator('h1')).toContainText('About Page')
 })
+
+function getByDataCy(selector: string) {
+  return `[data-cy=${selector}]`;
+}
+
+function getByForm(inputType:string, selector:string) {
+  return `[type="${inputType}"][name=${selector}]`;
+}
+
+
+test.describe('Authentication', () => {
+  test('should be redirected to auth page', async ({ page }) => {
+    await page.goto('/');
+    expect(await page.url()).toContain('/auth/sign-in');
+  });
+
+  test('should contain email, password, submit button and forgot password link', async ({ page }) => {
+    await page.goto('/auth/sign-in');
+
+    expect(await page.isVisible(getByForm('email', 'email'))).toBe(true);
+    expect(await page.isVisible(getByForm('password', 'password'))).toBe(true);
+    expect(await page.isVisible(getByDataCy('submit'))).toBe(true);
+    expect(await page.isVisible(getByDataCy('forgot_password'))).toBe(true);
+  });
+
+  test('should display an error message if form is empty', async ({ page }) => {
+    await page.click(getByDataCy('submit'));
+    
+    const emailInput = await page.$(getByForm('email', 'email'));
+    const emailValidation = await emailInput.checkValidity();
+    expect(emailValidation.valid).toBe(false);
+    expect(emailValidation.validationMessage).toContain('Please fill');
+
+    const passwordInput = await page.$(getByForm('password', 'password'));
+    const passwordValidation = await passwordInput.checkValidity();
+    expect(passwordValidation.valid).toBe(false);
+    expect(passwordValidation.validationMessage).toContain('Please fill');
+  });
+
+  test('should display an error message if user cannot connect', async ({ page }) => {
+    await page.fill(getByForm('email', 'email'), 'test@test.com');
+    await page.fill(getByForm('password', 'password'), process.env.ADMIN_PASSWORD);
+
+    await page.click(getByDataCy('submit'));
+    expect(await page.isVisible(getByDataCy('server_message'))).toBe(true);
+  });
+
+  test('should redirect user after successful login', async ({ page }) => {
+    await page.fill(getByForm('email', 'email'), 'dev@wavemind.ch');
+    await page.fill(getByForm('password', 'password'), process.env.ADMIN_PASSWORD);
+
+    await page.click(getByDataCy('submit'));
+    expect(await page.url()).toContain('/account/credentials');
+  });
+});
