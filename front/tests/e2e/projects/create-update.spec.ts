b86151cebd6821e37985531e574051fabb529505
Quentin Girard <quentin_girard@hotmail.com>
2023-08-24 15:00:39 +0200

    Update e2e

--- front/tests/e2e/projects/create-update.spec.ts
+++ front/tests/e2e/projects/create-update.spec.ts
@@ -36,7 +36,7 @@ test.describe('Create and update project', () => {
   })
 
   test('should update an existing project', async ({ adminPage }) => {
-    await adminPage.getByTestId('project-menu').first().click()
+    await adminPage.getByTestId('project-menu-1').click()
     await adminPage.page.getByRole('menuitem', { name: 'Settings' }).click()
     await adminPage.fillInput('name', 'Renamed project')
     await adminPage.submitForm()
