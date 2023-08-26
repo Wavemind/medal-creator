b2d791a8a8deb1413fb2939562c46a75e68228b7
ManuBarchich <manu250493@yahoo.fr>
2023-08-26 20:42:39 +0200

    Test alias

--- api/app/mailers/application_mailer.rb
+++ api/app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: email_address_with_name('noreply@unisante.ch', 'Unisanté')
-  default attachment: File.read(File.join(Rails.root, 'app', 'assets', 'images', 'logo-black.png'))
   layout 'mailer'
 end
--- api/app/mailers/devise_mailer.rb
+++ api/app/mailers/devise_mailer.rb
@@ -1,5 +1,5 @@
 class DeviseMailer < Devise::Mailer
-
+  default from: email_address_with_name('noreply@unisante.ch', 'Unisanté')
   def reset_password_instructions(record, token, opts={})
     attachments.inline['logo-black.png'] = File.read(File.join(Rails.root, 'app', 'assets', 'images', 'logo-black.png'))
     super(record, token, opts)
