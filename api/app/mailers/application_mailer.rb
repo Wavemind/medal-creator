0f87e0b0d85b1fe8ce8ac41f8d662341af8c74f4
ManuBarchich <manu250493@yahoo.fr>
2023-08-26 19:17:32 +0200

    Fix sender and email attachment

--- api/app/mailers/application_mailer.rb
+++ api/app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@unisante.ch <Unisanté>'
+  default from: 'Unisanté <noreply@unisante.ch>'
   default attachment: File.read(File.join(Rails.root, 'app', 'assets', 'images', 'logo-black.png'))
   layout 'mailer'
 end
