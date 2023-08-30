8e4e8b191d4e4213cce791ed4fce3d46fcd2a7c7
ManuBarchich <manu250493@yahoo.fr>
2023-08-30 11:37:47 +0200

    Fix specs using attachments

--- api/app/mailers/devise_mailer.rb
+++ api/app/mailers/devise_mailer.rb
@@ -1,9 +1,13 @@
 class DeviseMailer < Devise::Mailer
   default from: email_address_with_name('noreply@unisante.ch', 'medAL-creator')
-  default attachment: File.read(File.join(Rails.root, 'app', 'assets', 'images', 'logo-black.png'))
+
+  def invitation_instructions(*args)
+    attachments.inline['logo-black.png'] = File.read(File.join(Rails.root, 'app', 'assets', 'images', 'logo-black.png'))
+    super
+  end
+
   def reset_password_instructions(record, token, opts={})
     attachments.inline['logo-black.png'] = File.read(File.join(Rails.root, 'app', 'assets', 'images', 'logo-black.png'))
     super(record, token, opts)
   end
-
 end
