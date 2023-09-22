d80cc0658de8525ad864eab4a95e868655a0a7e2
ManuBarchich <manu250493@yahoo.fr>
2023-09-22 10:08:31 +0200

    Fix duplicate cc condition

--- api/app/models/node_complaint_category.rb
+++ api/app/models/node_complaint_category.rb
@@ -2,4 +2,6 @@
 class NodeComplaintCategory < ApplicationRecord
   belongs_to :node, optional: true
   belongs_to :complaint_category, class_name: 'Node', foreign_key: 'complaint_category_id'
+
+  validates :node_id, uniqueness: { scope: :complaint_category_id, message: I18n.t('activerecord.errors.node_complaint_categories.unique') }
 end
