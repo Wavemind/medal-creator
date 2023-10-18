8e20cb0edc09b6761ea1dac825ef243126413653
ManuBarchich <manu250493@yahoo.fr>
2023-10-18 05:55:49 +0200

    Add mutation

---
+++ api/app/graphql/mutations/algorithms/import_translations.rb
@@ -0,0 +1,37 @@
+module Mutations
+  module Algorithms
+    class ImportTranslations < Mutations::BaseMutation
+      # Fields
+      field :id, ID, null: true
+
+      # Arguments
+      argument :id, ID, required: true
+      argument :translations_file, ApolloUploadServer::Upload, required: false
+
+      # Works with current_user
+      def authorized?(id:, translations_file:)
+        algorithm = Algorithm.find(id)
+        return true if context[:current_api_v2_user].clinician? || context[:current_api_v2_user].user_projects.where(
+          project_id: algorithm.project_id, is_admin: true
+        ).any?
+
+        raise GraphQL::ExecutionError, I18n.t('graphql.errors.wrong_access', class_name: 'Algorithm')
+      rescue ActiveRecord::RecordNotFound => e
+        GraphQL::ExecutionError.new(I18n.t('graphql.errors.object_not_found', class_name: e.model))
+      end
+
+      # Resolve
+      def resolve(id:, translations_file:)
+        begin
+          if translations_file.present?
+            ImportTranslationsService.process(translations_file)
+          else
+
+          end
+        rescue ActiveRecord::RecordInvalid => e
+          GraphQL::ExecutionError.new(e.record.errors.to_json)
+        end
+      end
+    end
+  end
+end
