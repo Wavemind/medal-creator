63998ee7fd3f01d760b7df7ba2aa466651eeb508
Quentin Girard <quentin_girard@hotmail.com>
2023-10-24 13:45:23 +0200

    Update schema.rb

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -72,8 +72,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_09_074518) do
     t.json "medal_r_json"
     t.integer "medal_r_json_version", default: 0
     t.string "job_id", default: ""
-    t.datetime "first_published_at"
-    t.datetime "last_published_at"
+    t.datetime "published_at"
+    t.datetime "json_generated_at"
     t.datetime "archived_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
