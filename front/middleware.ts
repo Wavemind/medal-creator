2315711bed478bc479c2d5ae4f644ee5c0722454
Sinan Ucak <ucak.sinan@gmail.com>
2023-10-31 15:01:35 +0100

    improved and cleaned middleware

--- front/middleware.ts
+++ front/middleware.ts
@@ -1,9 +1,8 @@
 /**
  * The external imports
  */
-import { getToken } from 'next-auth/jwt'
-import { withAuth } from 'next-auth/middleware'
-import { i18n } from 'next-i18next'
+import { getToken, JWT } from 'next-auth/jwt'
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 
 /**
@@ -11,19 +10,15 @@ import { NextResponse } from 'next/server'
  */
 import { RoleEnum } from '@/types'
 
-export default withAuth(async function middleware(req) {
-  const token = await getToken({ req })
-  // If nextAuth contains a token
-  if (token) {
-    const role = token.user.role
-
+// Validate token by the api
+async function validateToken(token: JWT) {
+  try {
     const tokenValidityRequest = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/api/v2/auth/validate_token`,
       {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          'Accept-Language': i18n?.language || 'en',
           'access-token': token.accessToken!,
           client: token.client!,
           expiry: String(token.expiry!),
@@ -33,11 +28,73 @@ export default withAuth(async function middleware(req) {
     )
 
     const tokenValidityResponse = await tokenValidityRequest.json()
+    return tokenValidityResponse.success
+  } catch {
+    return false
+  }
+}
+
+// Fetch the current project from the api
+async function fetchProject(token: JWT, projectId: string) {
+  const projectRequest = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': token.accessToken!,
+        client: token.client!,
+        expiry: String(token.expiry!),
+        uid: token.uid!,
+      },
+      body: JSON.stringify({
+        query: `
+          query getProject($id: ID!) {
+            getProject(id: $id) {
+              id
+              isCurrentUserAdmin
+            }
+          }
+        `,
+        variables: {
+          id: projectId,
+        },
+      }),
+    }
+  )
+
+  const projectResponse = await projectRequest.json()
+  return projectResponse
+}
+
+// Sign out from nextAuth and API
+async function logout(req: NextRequestWithAuth) {
+  await fetch(`${process.env.NEXT_PUBLIC_FRONT_URL}/api/auth/signout`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  return NextResponse.redirect(
+    new URL('/auth/sign-in?notifications=session-expired', req.url)
+  )
+}
+
+export default withAuth(async function middleware(req) {
+  const token = await getToken({ req })
+
+  // If nextAuth contains a token
+  if (token) {
+    // Check token validity
+    const isTokenValid = await validateToken(token)
 
     // If the token is valid
-    if (tokenValidityResponse.success) {
-      // Only Admins are allowed to access /users
-      if (req.nextUrl.pathname === '/users') {
+    if (isTokenValid) {
+      const role = token.user.role
+      const pathname = req.nextUrl.pathname
+
+      // Only Admins are allowed to access /users and /projects/new
+      if (['/users', '/projects/new'].includes(pathname)) {
         if (role === RoleEnum.Admin) {
           return NextResponse.next()
         }
@@ -45,57 +102,25 @@ export default withAuth(async function middleware(req) {
       }
 
       // If the user is navigating in a specific project tree
-      if (req.nextUrl.pathname.includes('projects')) {
-        // Only Admins are allowed to access /projects/new
-        if (req.nextUrl.pathname === '/projects/new') {
-          if (role === RoleEnum.Admin) {
-            return NextResponse.next()
-          }
-          return NextResponse.redirect(new URL('/404', req.url))
-        }
-
-        const matchedText = req.nextUrl.pathname.match(/\/projects\/(\d+)/)
+      if (pathname.includes('projects')) {
+        const matchedText = pathname.match(/\/projects\/(\d+)/)
 
         // If the pathname contains numbers after project/
         if (matchedText) {
           const projectId = matchedText[1]
-
-          const projectRequest = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
-            {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Accept-Language': i18n?.language || 'en',
-                'access-token': token.accessToken!,
-                client: token.client!,
-                expiry: String(token.expiry!),
-                uid: token.uid!,
-              },
-              body: JSON.stringify({
-                query: `
-                  query getProject($id: ID!) {
-                    getProject(id: $id) {
-                      id
-                      isCurrentUserAdmin
-                    }
-                  }
-                `,
-                variables: {
-                  id: projectId,
-                },
-              }),
-            }
-          )
-
-          const projectResponse = await projectRequest.json()
+          const projectResponse = await fetchProject(token, projectId)
 
           // If the project response is successful and data is not null
           if (projectResponse.data) {
             const { isCurrentUserAdmin } = projectResponse.data.getProject
 
             // Only Admins and Project Admins are allowed to access /projects/:id/edit
-            if (req.nextUrl.pathname.includes('/edit')) {
+            // and /projects/:id/algorithms/:id/medal-data-config
+            if (
+              ['/edit', '/medal-data-config'].some(path =>
+                pathname.includes(path)
+              )
+            ) {
               if (isCurrentUserAdmin) {
                 return NextResponse.next()
               }
@@ -103,7 +128,7 @@ export default withAuth(async function middleware(req) {
             }
 
             // Only Admins, Project Admins and Deployment Managers are allowed to access /projects/:id/publication
-            if (req.nextUrl.pathname.includes('publication')) {
+            if (pathname.includes('/publication')) {
               if (isCurrentUserAdmin || role === RoleEnum.DeploymentManager) {
                 return NextResponse.next()
               }
@@ -111,16 +136,8 @@ export default withAuth(async function middleware(req) {
             }
 
             // Only Admins, Project Admins and Clinicians are allowed to access /projects/:id/algorithms/:id/exports
-            if (req.nextUrl.pathname.includes('exports')) {
+            if (pathname.includes('/exports')) {
               if (isCurrentUserAdmin || role === RoleEnum.Clinician) {
-                return NextResponse.next()
-              }
-              return NextResponse.redirect(new URL('/404', req.url))
-            }
-
-            // Only Admins and Project Admins are allowed to access /projects/:id/algorithms/:id/medal-data-config
-            if (req.nextUrl.pathname.includes('medal-data-config')) {
-              if (isCurrentUserAdmin) {
                 return NextResponse.next()
               }
               return NextResponse.redirect(new URL('/404', req.url))
@@ -140,15 +157,7 @@ export default withAuth(async function middleware(req) {
       return NextResponse.next()
     } else {
       // Sign out from nextAuth and API
-      await fetch(`${process.env.NEXT_PUBLIC_FRONT_URL}/api/auth/signout`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      return NextResponse.redirect(
-        new URL('/auth/sign-in?notifications=session-expired', req.url)
-      )
+      logout(req)
     }
   } else {
     // If nextAuth does not contain a token
