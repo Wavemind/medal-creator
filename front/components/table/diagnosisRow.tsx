5dac0a56cba5517a1941e0e748e2b67fb72e8f51
Quentin Girard <quentin_girard@hotmail.com>
2023-11-01 10:15:47 +0100

    Split decisionTreeRow in 2 files

---
+++ front/components/table/diagnosisRow.tsx
@@ -0,0 +1,235 @@
+/**
+ * The external imports
+ */
+import React, { useState, useCallback, useEffect } from 'react'
+import {
+  Table,
+  VStack,
+  Tr,
+  Td,
+  Button,
+  Skeleton,
+  Tbody,
+  Highlight,
+  Text,
+  Box,
+  Th,
+  Thead,
+} from '@chakra-ui/react'
+import { useTranslation } from 'next-i18next'
+
+/**
+ * The internal imports
+ */
+import MenuCell from '@/components/table/menuCell'
+import DiagnosisDetail from '@/components/modal/diagnosisDetail'
+import DiagnosisForm from '@/components/forms/diagnosis'
+import DiagramButton from '@/components/diagramButton'
+import {
+  useDestroyDiagnosisMutation,
+  useGetDiagnosesQuery,
+} from '@/lib/api/modules/enhanced/diagnosis.enhanced'
+
+import {
+  useAlertDialog,
+  useAppRouter,
+  useModal,
+  useProject,
+  useToast,
+} from '@/lib/hooks'
+import { LEVEL_OF_URGENCY_GRADIENT } from '@/lib/config/constants'
+import { extractTranslation } from '@/lib/utils/string'
+import type { DiagnosisRowComponent, Scalars } from '@/types'
+
+const DiagnosisRow: DiagnosisRowComponent = ({
+  decisionTreeId,
+  searchTerm,
+}) => {
+  const { t } = useTranslation('datatable')
+  const { newToast } = useToast()
+  const { isAdminOrClinician } = useProject()
+
+  const { projectLanguage } = useProject()
+  const { open: openModal } = useModal()
+  const { open: openAlertDialog } = useAlertDialog()
+
+  const {
+    query: { algorithmId, projectId },
+  } = useAppRouter()
+
+  const { data: diagnoses, isLoading } = useGetDiagnosesQuery({
+    algorithmId: algorithmId,
+    decisionTreeId,
+  })
+
+  const [
+    destroyDiagnosis,
+    { isSuccess: isDiagnosisDestroySuccess, isError: isDiagnosisDestroyError },
+  ] = useDestroyDiagnosisMutation()
+
+  /**
+   * Callback to handle the new form action in the table menu for a new diagnosis
+   */
+  const onNewDiagnosis = useCallback(() => {
+    openModal({
+      title: t('new', { ns: 'diagnoses' }),
+      content: <DiagnosisForm decisionTreeId={decisionTreeId} />,
+    })
+  }, [])
+
+  /**
+   * Callback to handle the new form action in the table menu for a new diagnosis
+   */
+  const onEditDiagnosis = useCallback((diagnosisId: Scalars['ID']) => {
+    openModal({
+      title: t('edit', { ns: 'diagnoses' }),
+      content: <DiagnosisForm diagnosisId={diagnosisId} />,
+    })
+  }, [])
+
+  /**
+   * Callback to handle the suppression of a decision tree
+   */
+  const onDiagnosisDestroy = useCallback((diagnosisId: Scalars['ID']) => {
+    openAlertDialog({
+      title: t('delete'),
+      content: t('areYouSure', { ns: 'common' }),
+      action: () => destroyDiagnosis({ id: diagnosisId }),
+    })
+  }, [])
+
+  /**
+   * Callback to handle the info action in the table menu
+   */
+  const onInfo = useCallback((diagnosisId: Scalars['ID']) => {
+    openModal({
+      content: <DiagnosisDetail diagnosisId={diagnosisId} />,
+    })
+  }, [])
+
+  useEffect(() => {
+    if (isDiagnosisDestroySuccess) {
+      newToast({
+        message: t('notifications.destroySuccess', { ns: 'common' }),
+        status: 'success',
+      })
+    }
+  }, [isDiagnosisDestroySuccess])
+
+  useEffect(() => {
+    if (isDiagnosisDestroyError) {
+      newToast({
+        message: t('notifications.destroyError', { ns: 'common' }),
+        status: 'error',
+      })
+    }
+  }, [isDiagnosisDestroyError])
+
+  return (
+    <Tr>
+      <Td p={0} colSpan={5} pl={8} bg='gray.100'>
+        <Table data-testid='diagnoses-row'>
+          <Thead>
+            <Tr>
+              <Th>{t('diagnoses.diagnosis')}</Th>
+              <Th>{t('diagnoses.levelOfUrgency')}</Th>
+            </Tr>
+          </Thead>
+          {isLoading ? (
+            <Tbody>
+              <Tr>
+                <Td colSpan={3}>
+                  <Skeleton h={10} />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td colSpan={3}>
+                  <Skeleton h={10} />
+                </Td>
+              </Tr>
+            </Tbody>
+          ) : (
+            <Tbody w='full'>
+              {diagnoses?.edges.length === 0 && (
+                <Tr>
+                  <Td colSpan={4}>
+                    <Text fontWeight='normal'>{t('noData')}</Text>
+                  </Td>
+                </Tr>
+              )}
+              {diagnoses?.edges.map(edge => (
+                <Tr
+                  key={`diagnosis-${edge.node.id}`}
+                  data-testid='diagnosis-row'
+                >
+                  <Td borderColor='gray.300' w='50%'>
+                    <VStack alignItems='left'>
+                      <Text fontSize='sm' fontWeight='light'>
+                        {edge.node.fullReference}
+                      </Text>
+                      <Text>
+                        <Highlight
+                          query={searchTerm}
+                          styles={{ bg: 'red.100' }}
+                        >
+                          {extractTranslation(
+                            edge.node.labelTranslations,
+                            projectLanguage
+                          )}
+                        </Highlight>
+                      </Text>
+                    </VStack>
+                  </Td>
+                  <Td borderColor='gray.300'>
+                    <Box
+                      borderRadius='full'
+                      height={8}
+                      width={8}
+                      display='flex'
+                      justifyContent='center'
+                      alignItems='center'
+                      bg={
+                        LEVEL_OF_URGENCY_GRADIENT[edge.node.levelOfUrgency - 1]
+                      }
+                    >
+                      {edge.node.levelOfUrgency}
+                    </Box>
+                  </Td>
+                  <Td borderColor='gray.300' textAlign='center'>
+                    <DiagramButton
+                      href={`/projects/${projectId}/diagram/diagnosis/${edge.node.id}`}
+                    >
+                      {t('openTreatment')}
+                    </DiagramButton>
+                  </Td>
+                  <Td textAlign='right' borderColor='gray.300'>
+                    <MenuCell
+                      itemId={edge.node.id}
+                      onInfo={onInfo}
+                      onEdit={isAdminOrClinician ? onEditDiagnosis : undefined}
+                      onDestroy={
+                        isAdminOrClinician ? onDiagnosisDestroy : undefined
+                      }
+                      canDestroy={!edge.node.hasInstances}
+                    />
+                  </Td>
+                </Tr>
+              ))}
+              <Tr>
+                {isAdminOrClinician && (
+                  <Td colSpan={4} textAlign='center'>
+                    <Button variant='outline' onClick={onNewDiagnosis}>
+                      {t('addDiagnosis')}
+                    </Button>
+                  </Td>
+                )}
+              </Tr>
+            </Tbody>
+          )}
+        </Table>
+      </Td>
+    </Tr>
+  )
+}
+
+export default DiagnosisRow
