9abdfccf372e91f0879a397efdd0fd557ea0a115
Quentin Girard <quentin_girard@hotmail.com>
2023-11-20 13:56:55 +0100

    Fix issue when elapsed time is not display if less than 1 second

--- front/lib/utils/date.ts
+++ front/lib/utils/date.ts
@@ -6,6 +6,8 @@ import { format, formatDuration, intervalToDuration } from 'date-fns'
 export const formatDate = (date: Date): string => format(date, 'dd.LL.yyyy')
 
 export const customFormatDuration = (elapsedTime: number): string => {
-  const durations = intervalToDuration({ start: 0, end: elapsedTime * 1000 })
+  if (elapsedTime === 0) return ''
+  const timming = elapsedTime > 1 ? elapsedTime : 1
+  const durations = intervalToDuration({ start: 0, end: timming * 1000 })
   return formatDuration(durations)
 }
