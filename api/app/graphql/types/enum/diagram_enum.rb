936a6227a9fa00be5fdfcbee352c98347cd77557
ManuBarchich <manu250493@yahoo.fr>
2023-12-03 22:24:52 +0100

    Accept QuestionsSequenceScored as a diagram enum value

--- api/app/graphql/types/enum/diagram_enum.rb
+++ api/app/graphql/types/enum/diagram_enum.rb
@@ -5,6 +5,11 @@ module Types
       value 'DecisionTree'
       value 'Diagnosis'
       value 'QuestionsSequence'
+      value 'QuestionsSequenceScored'
+
+      def self.coerce_input(input_value, _context)
+        input_value == 'QuestionsSequenceScored' ? 'QuestionsSequences::Scored' : input_value
+      end
     end
   end
 end
