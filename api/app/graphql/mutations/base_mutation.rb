1fae9c9ab448068f6d9133c6ad74eef46b64de7f
ManuBarchich <manu250493@yahoo.fr>
2023-12-15 15:51:58 +0100

    Fix from PR

--- api/app/graphql/mutations/base_mutation.rb
+++ api/app/graphql/mutations/base_mutation.rb
@@ -4,5 +4,15 @@ module Mutations
     field_class Types::BaseField
     input_object_class Types::BaseInputObject
     object_class Types::BaseObject
+
+    def check_deployed_instance(instance)
+      if instance.instanceable_type == 'Algorithm'
+        raise GraphQL::ExecutionError, I18n.t('graphql.errors.deployed_algorithm', status: instance.instanceable.status) if instance.instanceable.prod?
+      elsif instance.instanceable_type == 'DecisionTree'
+        raise GraphQL::ExecutionError, I18n.t('graphql.errors.deployed_algorithm', status: instance.instanceable.algorithm.status) if instance.instanceable.algorithm.prod?
+      else
+        raise GraphQL::ExecutionError, I18n.t('graphql.errors.deployed_node') if instance.instanceable.is_deployed?
+      end
+    end
   end
 end
