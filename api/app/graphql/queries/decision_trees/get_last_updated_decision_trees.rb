20838003a4dddf60fe61bdebf834601b86e8ccbd
ManuBarchich <manu250493@yahoo.fr>
2023-12-18 10:00:54 +0100

    Add search_term in recents query

--- api/app/graphql/queries/decision_trees/get_last_updated_decision_trees.rb
+++ api/app/graphql/queries/decision_trees/get_last_updated_decision_trees.rb
@@ -3,17 +3,23 @@ module Queries
     class GetLastUpdatedDecisionTrees < Queries::BaseQuery
       type Types::DecisionTreeType.connection_type, null: false
       argument :project_id, ID, required: true
+      argument :search_term, String, required: false
 
       # Works with current_user
-      def authorized?(project_id:)
+      def authorized?(project_id:, search_term: '')
         return true if context[:current_api_v2_user].has_access_to_project?(project_id)
 
         raise GraphQL::ExecutionError, I18n.t('graphql.errors.wrong_access', class_name: 'Project')
       end
 
-      def resolve(project_id:)
+      def resolve(project_id:, search_term: '')
         project = Project.find(project_id)
-        DecisionTree.where(algorithm: project.algorithms).includes(:algorithm, :node).order(updated_at: :desc)
+        if search_term.present?
+          DecisionTree.where(algorithm: project.algorithms).includes(:algorithm, :node).order(updated_at: :desc)
+                      .search(search_term, project.language.code)
+        else
+          DecisionTree.where(algorithm: project.algorithms).includes(:algorithm, :node).order(updated_at: :desc)
+        end
       rescue ActiveRecord::RecordNotFound => e
         GraphQL::ExecutionError.new(I18n.t('graphql.errors.object_not_found', class_name: e.record.class))
       rescue ActiveRecord::RecordInvalid => e
