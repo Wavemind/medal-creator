04d4a436645ade2600618b4a184410fc14b6323e
ManuBarchich <manu250493@yahoo.fr>
2024-02-05 22:58:53 +0100

    Remove unused type

--- api/app/graphql/types/algorithm_validation_type.rb
+++
@@ -1,6 +0,0 @@
-module Types
-  class AlgorithmValidationType < Types::BaseObject
-    field :invalid_decision_trees, [Types::DecisionTreeType], null: false
-    field :missing_nodes, [Types::NodeType], null: false
-  end
-end
